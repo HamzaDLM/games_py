6c39966b9c17b93e1afb53b75e4f250841db9f74
HamzaDLM <hamzadellam@hotmail.com>
2023-05-22 01:26:01 +0100

    grid and clear button

--- neural_net/main.go
+++ neural_net/main.go
@@ -25,8 +25,8 @@ func makeGrid(g Grid, l [][]uint8) {
 		for j := 0; j < g.size; j++ {
 			cx := int32(g.x + g.size + (i * g.size))
 			cy := int32(g.y + g.size + (j * g.size))
-			rl.DrawCircle(cx, cy, float32((g.r)-2), color.RGBA{255, 255, 255, l[i][j]})
-			rl.DrawCircleLines(cx, cy, float32((g.r)-2), color.RGBA{255, 255, 255, 100})
+			rl.DrawCircle(cx, cy, float32(g.r-2), color.RGBA{255, 255, 255, l[i][j]})
+			rl.DrawCircleLines(cx, cy, float32(g.r-2), color.RGBA{255, 255, 255, 100})
 		}
 	}
 }
@@ -49,9 +49,8 @@ func main() {
 	// set FPS
 	rl.SetTargetFPS(40)
 
-	gridSize := 20
-	inputArray := initializeArray(gridSize)
-
+	gridInfo := Grid{size: 20, r: 10, x: 100, y: 200}
+	inputArray := initializeArray(gridInfo.size)
 	// Main game loop
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
@@ -59,11 +58,11 @@ func main() {
 		pos := rl.GetMousePosition()
 
 		if rl.IsMouseButtonDown(0) {
-			for i := 0; i < gridSize; i++ {
-				for j := 0; j < gridSize; j++ {
-					cx := int32(100 + gridSize + (i * gridSize))
-					cy := int32(200 + gridSize + (j * gridSize))
-					if int32(pos.X) > cx && int32(pos.X) < cx+10 && int32(pos.Y) > cy && int32(pos.Y) < cy+10 {
+			for i := 0; i < gridInfo.size; i++ {
+				for j := 0; j < gridInfo.size; j++ {
+					cx := int32(gridInfo.x + gridInfo.size + (i * gridInfo.size))
+					cy := int32(gridInfo.y + gridInfo.size + (j * gridInfo.size))
+					if int32(pos.X) > cx && int32(pos.X) < cx+int32(gridInfo.r) && int32(pos.Y) > cy && int32(pos.Y) < cy+int32(gridInfo.r) {
 						inputArray[i][j] = 255
 					}
 				}
@@ -71,13 +70,13 @@ func main() {
 		}
 
 		rl.DrawText("Drag to write a number", 190, 170, 20, rl.White)
-		makeGrid(Grid{size: gridSize, r: 10, x: 100, y: 200}, inputArray)
+		makeGrid(gridInfo, inputArray)
 		// Grid clear button
 		// TODO make the values into variables
 		if rl.IsMouseButtonDown(0) && pos.X > 225 && pos.X < 375 && pos.Y > 650 && pos.Y < 680 {
 			rl.DrawRectangleLines(225, 650, 150, 30, rl.White)
 			rl.DrawText("Clear Grid", 255, 655, 18, rl.White)
-			inputArray = initializeArray(gridSize)
+			inputArray = initializeArray(gridInfo.size)
 		} else {
 			rl.DrawRectangle(225, 650, 150, 30, rl.Gray)
 			rl.DrawText("Clear Grid", 255, 655, 18, rl.Black)
